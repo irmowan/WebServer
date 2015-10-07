d7ede688bd7d69bc3d082afe77a8f99f5ca85403
irmowan <13307130174@fudan.edu.cn>
2015-10-07 21:23:43 +0800

    Add some annotations

--- Client.c
+++ Client.c
@@ -1,6 +1,6 @@
 //
 // Created by irmo on 15/10/7.
-//
+// Reference to http://www.linuxhowtos.org/data/6/client.c
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,51 +11,54 @@
 #include <netinet/in.h>
 #include <netdb.h>
 
-void error(const char *msg)
-{
+void error(const char *msg) {
     perror(msg);
     exit(0);
 }
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     int sockfd, portno, n;
     struct sockaddr_in serv_addr;
+    // The hostent structure defines a host computer on the Internet.
     struct hostent *server;
 
     char buffer[256];
     if (argc < 3) {
-        fprintf(stderr,"usage %s hostname port\n", argv[0]);
+        fprintf(stderr, "usage %s hostname port\n", argv[0]);
         exit(0);
     }
     portno = atoi(argv[2]);
+    // socket() creates a new socket.
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    // If the socket call fails, it returns -1.
     if (sockfd < 0)
         error("ERROR opening socket");
     server = gethostbyname(argv[1]);
     if (server == NULL) {
-        fprintf(stderr,"ERROR, no such host\n");
+        fprintf(stderr, "ERROR, no such host\n");
         exit(0);
     }
     bzero((char *) &serv_addr, sizeof(serv_addr));
+    // Set the field in serv_addr.
     serv_addr.sin_family = AF_INET;
-    bcopy((char *)server->h_addr,
-          (char *)&serv_addr.sin_addr.s_addr,
-          server->h_length);
     serv_addr.sin_port = htons(portno);
-    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0)
+    bcopy((char *) server->h_addr, (char *) &serv_addr.sin_addr.s_addr, server->h_length);
+    // connect() establishes a connection to the server.
+    if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
         error("ERROR connecting");
+    // Prompts the user to enter a message, and send to the server through socket,
+    // then get the reply from server and show it.
     printf("Please enter the message: ");
-    bzero(buffer,256);
-    fgets(buffer,255,stdin);
-    n = write(sockfd,buffer,strlen(buffer));
+    bzero(buffer, 256);
+    fgets(buffer, 255, stdin);
+    n = write(sockfd, buffer, strlen(buffer));
     if (n < 0)
         error("ERROR writing to socket");
-    bzero(buffer,256);
-    n = read(sockfd,buffer,255);
+    bzero(buffer, 256);
+    n = read(sockfd, buffer, 255);
     if (n < 0)
         error("ERROR reading from socket");
-    printf("%s\n",buffer);
+    printf("%s\n", buffer);
     close(sockfd);
     return 0;
 }
--- Server.c
+++ Server.c
@@ -1,6 +1,6 @@
 //
 // Created by irmo on 15/10/7.
-//
+// Reference to http://www.linuxhowtos.org/data/6/server.c
 
 /* A simple server in the internet domain using TCP
    The port number is passed as an argument */
@@ -13,46 +13,54 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
-void error(const char *msg)
-{
+void error(const char *msg) {
     perror(msg);
     exit(1);
 }
 
-int main(int argc, char *argv[])
-{
-    int sockfd, newsockfd, portno;
+int main(int argc, char *argv[]) {
+    int sockfd, newsockfd, portno, n;
     socklen_t clilen;
     char buffer[256];
+    // sockaddr_in is a structure containing an internet address.
     struct sockaddr_in serv_addr, cli_addr;
-    int n;
     if (argc < 2) {
-        fprintf(stderr,"ERROR, no port provided\n");
+        fprintf(stderr, "ERROR, no port provided\n");
         exit(1);
     }
+    // socket() creates a new socket.
+    // AF_INET is the address domain of the socket, which means the Internet domain for two hosts.
+    // SOCK_STREAM is a type of socket, as a stream.
+    // The third argument is the protocol, zero let the operating system choose the most appropriate protocol.
+    // Here it will choose TCP for stream sockets.
+    // The socket call returns a entry into the file descriptor table.
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    // If the socket call fails, it returns -1.
     if (sockfd < 0)
         error("ERROR opening socket");
     bzero((char *) &serv_addr, sizeof(serv_addr));
     portno = atoi(argv[1]);
+    // htons() converts a port number in host byte order to in network byte order.
     serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = INADDR_ANY;
     serv_addr.sin_port = htons(portno);
-    if (bind(sockfd, (struct sockaddr *) &serv_addr,
-             sizeof(serv_addr)) < 0)
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    // bind() binds a socket to an address.
+    if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
         error("ERROR on binding");
-    listen(sockfd,5);
+    // listen() applies to stream sockets.
+    // The second argument is the size of backlog queue,
+    // 5 is the maximum size permitted by most systems.
+    listen(sockfd, 5);
     clilen = sizeof(cli_addr);
-    newsockfd = accept(sockfd,
-                       (struct sockaddr *) &cli_addr,
-                       &clilen);
+    // accept() causes the process to block until a client connects to the server.
+    newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
     if (newsockfd < 0)
         error("ERROR on accept");
-    bzero(buffer,256);
-    n = read(newsockfd,buffer,255);
+    bzero(buffer, 256);
+    n = read(newsockfd, buffer, 255);
     if (n < 0) error("ERROR reading from socket");
-    printf("Here is the message: %s\n",buffer);
-    n = write(newsockfd,"I got your message",18);
+    printf("Here is the message: %s\n", buffer);
+    n = write(newsockfd, "I got your message", 18);
     if (n < 0) error("ERROR writing to socket");
     close(newsockfd);
     close(sockfd);
